3d5a0c198488c7e3098a853bb7c96413a20f92b9
haseeb ali <haseeb@apomatix.com>
2020-11-16 18:23:40 +0000

    Updated tailwind config to purge css

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -3,6 +3,20 @@ import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import Slider from "react-slick"
 import JohnImage from "../assets/img/john-photo.png"
+import DennisImage from "../assets/img/dennis.png"
+import GurgenImage from "../assets/img/gurgen.png"
+import MeiImage from "../assets/img/mei.png"
+import RobImage from "../assets/img/robert-w.png"
+
+let testimonials = [
+  {
+    comment:
+      "Professional team, and easy to use platform, allowed us to identify risks, apply appropriate solutions, and future-proof our business. This has put us in firm control of our compliance activities across our whole business. we're happy to have Apomatix by our side.",
+    image: JohnImage,
+    name: "John Williams",
+    company: "Equiti Global Markets",
+  },
+]
 
 const Testimonials = () => {
   const settings = {
@@ -16,32 +30,35 @@ const Testimonials = () => {
   }
   return (
     <Slider {...settings}>
-      <div className="text-center mr-8">
+      <div className="text-center pr-8">
         <div className="text-sm bg-white relative mb-12 p-5 rounded shadow-ax comment">
           Professional team, and easy to use platform, allowed us to identify
           risks, apply appropriate solutions, and future-proof our business.
           This has put us in firm control of our compliance activities across
           our whole business. we're happy to have Apomatix by our side.
         </div>
-        <div className="mb-5 flex justify-center">
-          <img src={JohnImage} className="w-20 h-20 rounded-full" />
+        <div className="mb-5">
+          <img src={JohnImage} className="w-20 h-20 rounded-full mx-auto" />
+        </div>
+        <div>
+          <span className="block text-base">John Williams</span>
+          <span className="block text-sm">Equiti Global Markets</span>
         </div>
       </div>
-      <div className="border border-red-500 text-center">
-        <h3>2</h3>
-      </div>
-      <div className="border border-red-500 text-center">
-        <h3>3</h3>
-      </div>
-      <div className="border border-red-500 text-center">
-        <h3>4</h3>
-      </div>
-      <div className="border border-red-500 text-center">
-        <h3>5</h3>
-      </div>
-      <div className="border border-red-500 text-center">
-        <h3>6</h3>
-      </div>
+      {testimonials.map(x => (
+        <div className="text-center pr-8">
+          <div className="text-sm bg-white relative mb-12 p-5 rounded shadow-ax comment">
+            {x.comment}
+          </div>
+          <div className="mb-5">
+            <img src={x.image} className="w-20 h-20 rounded-full mx-auto" />
+          </div>
+          <div>
+            <span className="block text-base">{x.name}</span>
+            <span className="block text-sm">{x.company}</span>
+          </div>
+        </div>
+      ))}
     </Slider>
   )
 }
