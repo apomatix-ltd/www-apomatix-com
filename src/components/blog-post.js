c0fc684de4bb416c7f4e89c24b86493992ae01a8
haseeb ali <haseeb@apomatix.com>
2020-11-30 14:29:37 +0000

    Updated blog styling and images

--- src/components/blog-post.js
+++ src/components/blog-post.js
@@ -3,68 +3,87 @@ import { Link } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
 import { normalizePath } from "../utils/get-url-path"
-import { format } from 'date-fns'
+import { format } from "date-fns"
 import SEO from "../components/seo"
 import Button from "../components/Button"
 
 function BlogPost({ data }) {
   const { nextPage, previousPage, page } = data
   const { title, content, author, date, featuredImage, categories } = page
- 
-  var cat = "";    
-  categories.nodes.forEach(n => cat += (' ' + n.name));
+
+  var cat = ""
+  categories.nodes.forEach(n => (cat += " " + n.name))
 
   return (
     <Layout className="page">
-  
       <section className="container mx-auto mt-32">
-      <div className="mb-10">  
-        <h1 className="text-4xl">{title}</h1> 
-        <h2 className="text-1xl"><b>Author:</b> {author.node.firstName + ' ' + author.node.lastName}</h2>
-        <h3 className="text-1xl"><b>Published:</b> {format(new Date(date), 'PPPPpp')}</h3>
-        <h3 className="text-1xl"><b>Categories:</b> {cat}</h3>
-      </div>
+        <div className="mb-10">
+          <h1 className="text-4xl font-medium">{title}</h1>
+          <h2 className="text-1xl">
+            <b>Author:</b> {author.node.firstName + " " + author.node.lastName}
+          </h2>
+          <h3 className="text-1xl">
+            <b>Published:</b> {format(new Date(date), "PPPPpp")}
+          </h3>
+          <h3 className="text-1xl">
+            <b>Categories:</b> {cat}
+          </h3>
+        </div>
         <div className="text-center mb-10">
           <Img
-            resolutions = {featuredImage.node.localFile.childImageSharp.resolutions}
+            resolutions={
+              featuredImage.node.localFile.childImageSharp.resolutions
+            }
           />
         </div>
-      <div className="leading-8">
-      <p dangerouslySetInnerHTML={{ __html: content }} />
-      </div>
-      <br />
-      {!!nextPage && (
-        <Link className="text-blue-700" to={normalizePath('/blog' + nextPage.uri)}>Next: {nextPage.title}</Link>
-      )}
-      <br />
-      {!!previousPage && (
-        <Link className="text-center text-blue-700" to={normalizePath('/blog' + previousPage.uri)}>
-          Previous: {previousPage.title}
-        </Link>
-      )}
+        <div className="leading-8">
+          <p dangerouslySetInnerHTML={{ __html: content }} />
+        </div>
+        <br />
+        {!!nextPage && (
+          <Link
+            className="text-blue-700"
+            to={normalizePath("/blog" + nextPage.uri)}
+          >
+            Next: {nextPage.title}
+          </Link>
+        )}
+        <br />
+        {!!previousPage && (
+          <Link
+            className="text-center text-blue-700"
+            to={normalizePath("/blog" + previousPage.uri)}
+          >
+            Previous: {previousPage.title}
+          </Link>
+        )}
       </section>
       <div className="mb-20"></div>
       <section className="parallax text-white">
-          <div className="relative">
-            <div className="bg-branding opacity-75 absolute inset-0 z-1" />
-            <div className="container mx-auto z-10 relative pt-24 pb-18">
-              <div className="grid grid-cols-1 md:grid-cols-12">
-                <div className="col-span-1 md:col-span-8 px-4 text-center md:text-left">
-                  <h2 className="text-2xl mb-5">Understand your risks. Reduce the impact. Protect your business.</h2>
-                  <p className="text-base mb-12">
-                   Apomatix’s Powerful Risk Management Software to help you understand, fix and manage all your organisation’s risks.
-                  </p>
-                </div>
-                <div className="col-span-1 md:col-span-4 px-4 flex items-center justify-center md:justify-end">
+        <div className="relative">
+          <div className="bg-branding opacity-75 absolute inset-0 z-1" />
+          <div className="container mx-auto z-10 relative pt-24 pb-18">
+            <div className="grid grid-cols-1 md:grid-cols-12">
+              <div className="col-span-1 md:col-span-8 px-4 text-center md:text-left">
+                <h2 className="text-2xl mb-5">
+                  Understand your risks. Reduce the impact. Protect your
+                  business.
+                </h2>
+                <p className="text-base mb-12">
+                  Apomatix’s Powerful Risk Management Software to help you
+                  understand, fix and manage all your organisation’s risks.
+                </p>
+              </div>
+              <div className="col-span-1 md:col-span-4 px-4 flex items-center justify-center md:justify-end">
                 <a href="https://app.apomatix.com/register/website-risk-trial">
-                    <Button type="primary">Start free trial</Button>
-                  </a>
-                </div>
+                  <Button type="primary">Start free trial</Button>
+                </a>
               </div>
             </div>
           </div>
-        </section>
-        <div className="mb-20"></div>
+        </div>
+      </section>
+      <div className="mb-20"></div>
     </Layout>
   )
 }
--- src/templates/wp.js
+++ src/templates/wp.js
@@ -5,86 +5,93 @@ import ReactPaginate from "react-paginate"
 import Button from "../components/Button"
 import Layout from "../components/layout"
 import { normalizePath } from "../utils/get-url-path"
-import { format } from 'date-fns'
+import { format } from "date-fns"
 import SEO from "../components/seo"
 
 export default ({ data, pageContext }) => (
   <Layout className="page">
-    
-    <SEO
-      title = 'ddddd'
-      description = 'Apomatix ddddddd'
-      article = 'ddddd'
-    />
+    <SEO title="ddddd" description="Apomatix ddddddd" article="ddddd" />
 
-    <section className="container mx-auto mt-32">
-    <div className="mb-10">
-      <h1 className="text-3xl">Apomatix Blog</h1>
-    </div>
-    <div className="grid grid-cols-1 gap-y-10">
-   
-      {data.allWpPost.nodes.map((page) => (
-        <div className="col-span-1" key={page.link}>
-          <Link to={normalizePath('/blog' + page.uri)}>
-            <div p={5} shadow="md" borderWidth="1px">
-              <div className="grid grid-cols-2">
-                <div className="col-span-1">
+    <section className="container mx-auto mt-40">
+      <div className="mb-20">
+        <h1 className="text-4xl font-medium text-center">Apomatix Blog</h1>
+      </div>
+      <div className="grid grid-cols-1 gap-y-10">
+        {data.allWpPost.nodes.map(page => (
+          <div className="col-span-1" key={page.link}>
+            <Link to={normalizePath("/blog" + page.uri)}>
+              <div className="grid grid-cols-1 lg:grid-cols-2">
+                <div className="col-span-1 flex justify-center px-4">
                   {!!page?.featuredImage?.node?.localFile?.childImageSharp && (
                     <Img
-                    resolutions={
-                        page.featuredImage.node.localFile.childImageSharp.resolutions
+                      fluid={
+                        page.featuredImage.node.localFile.childImageSharp.fluid
                       }
+                      className="w-full"
                     />
                   )}
                 </div>
-                <div className="col-span-1 flex items-center">
+                <div className="col-span-1 flex justify-center pt-10 lg:pt-0 lg:items-center px-4 mb-16 lg:mb-0">
                   <div className="text-xl">
                     <h2>{page.title} </h2>
-                    {!!page.author && !!page.author.node.firstName  && (
-                    <h3> <b>Author:</b> { page.author.node.firstName + ' ' +  page.author.node.lastName}</h3>
+                    {!!page.author && !!page.author.node.firstName && (
+                      <h3>
+                        {" "}
+                        <b>Author:</b>{" "}
+                        {page.author.node.firstName +
+                          " " +
+                          page.author.node.lastName}
+                      </h3>
                     )}
-                    <h3><b>Date:</b> {format(new Date(page.date), 'PPPPpp')}</h3>
+                    <h3>
+                      <b>Date:</b> {format(new Date(page.date), "PPPPpp")}
+                    </h3>
                   </div>
                 </div>
               </div>
-            </div>
-          </Link>
-        </div>
-      ))}
-    </div>
-    </section>
-    <section>
-    {pageContext && pageContext.totalPages > 1 && (
-      <div className="container mx-auto mr-10 mt-10">
-        <ReactPaginate
-          previousLabel={
-            pageContext?.page !== 1 && <Button>Previous page</Button>
-          }
-          nextLabel={
-            pageContext?.totalPages !== pageContext.page && (
-              <Button>Next page</Button>
-            )
-          }
-          onPageChange={({ selected }) => {
-            const page = selected + 1
-            const path = page === 1 ? `/blog/` : `/blog/${page}/`
-            navigate(path)
-          }}
-          disableInitialCallback
-          breakLabel={"..."}
-          breakClassName={"mx-20"}
-          pageCount={pageContext.totalPages}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          containerClassName={"flex flex-row"}
-          subContainerClassName={"mx-20"}
-          activeClassName={"active"}
-          pageClassName= {"mx-5"}
-          initialPage={pageContext.page - 1}
-        />
+            </Link>
+          </div>
+        ))}
       </div>
-    )}
-     </section>
+    </section>
+    <section className="container mx-auto">
+      {pageContext && pageContext.totalPages > 1 && (
+        <div className="my-12 px-4">
+          <ReactPaginate
+            previousLabel={
+              pageContext?.page !== 1 && (
+                <Button type="primary" outline={true}>
+                  Previous
+                </Button>
+              )
+            }
+            nextLabel={
+              pageContext?.totalPages !== pageContext.page && (
+                <Button type="primary" outline={true}>
+                  Next
+                </Button>
+              )
+            }
+            onPageChange={({ selected }) => {
+              const page = selected + 1
+              const path = page === 1 ? `/blog/` : `/blog/${page}/`
+              navigate(path)
+            }}
+            disableInitialCallback
+            breakLabel={"..."}
+            breakClassName={"mx-20"}
+            pageCount={pageContext.totalPages}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            containerClassName={"flex flex-row items-center flex-wrap"}
+            subContainerClassName={"mx-20"}
+            activeClassName={"text-branding border-b border-branding"}
+            pageClassName={"mx-5"}
+            initialPage={pageContext.page - 1}
+          />
+        </div>
+      )}
+    </section>
   </Layout>
 )
 
@@ -121,10 +128,16 @@ export const query = graphql`
             localFile {
               childImageSharp {
                 id
-                resolutions (width:478, height: 244) {
-                  src,
-                  width,
-                  height
+                fluid(
+                  maxWidth: 568
+                  maxHeight: 297
+                  quality: 80
+                  srcSetBreakpoints: [960, 1440]
+                ) {
+                  ...GatsbyImageSharpFluid
+                }
+                sizes {
+                  src
                 }
               }
             }
